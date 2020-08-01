ea1e4174d07dfa46d32af2e30fc91670837e0183
Dinushka Piyumal <57054631+dinushka114@users.noreply.github.com>
2020-08-01 21:27:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
 
-// fetch("https://covid19.mathdro.id/api/countries").
-// then(res => res.json()).
-// then(data => console.log(data.countries));
-const se = document.querySelector("#sel");
-const c_name = document.querySelector("#c_name")
-const tbody = document.querySelector("#tbody")
-class App{
-    // data = 
-    
-    getData(){
-        // let alldata 
-        fetch("https://covid19.mathdro.id/api/countries").
-        then(res => res.json()).
-        then(data => this.logData(data.countries));
 
-        // console.log(alldata);
-        // re
-    }
+class CovidApp{
 
-    logData(data){
-        // console.log(data);
-        let countriList = "<option>--select country--</option>"
-        data.forEach(countri => {
-            // console.log(countri);
-            countriList = countriList + "<option value = '"+countri.name+"' >"+countri.name+"</option>";
-        });
-        se.innerHTML = countriList;
+    fetchCountries(){
 
-    }
+        var selectSection = document.getElementById('countries');
+        var select = document.createElement('select');
+        select.className = 'form-control';
+        var countrySection  = document.createElement('div');
+        countrySection.className = 'countrySec';
+        selectSection.appendChild(countrySection);
 
-    getCountriStatus(c){
-        let countriName = c.value
-        // console.log(countriName);
-        fetch("https://covid19.mathdro.id/api/countries/"+countriName).then(res => res.json()).
-        then(data => this.getCountriStatusAll(countriName,data));
+        fetch('https://covid19.mathdro.id/api/countries')
+        .then(response => response.json())
+        .then(
+            data => data.countries.map((country)=>{
+                var option = document.createElement('option');
+                var optionValue = document.createTextNode(country.name);
+                option.appendChild(optionValue);
+                select.appendChild(option);
+                option.addEventListener('click' ,(event)=>{
+                    this.fetchCountryData(event.target.value);
+                })
+            },
+            countrySection.appendChild(select),
+            // countrySection.appendChild(selectSection)
+            )
+        );
 
     }
 
-    getCountriStatusAll(name,data){
-        c_name.innerHTML = name
-        tbody.innerHTML = "<tr><td>"+data.confirmed.value+"</td><td>"+data.recovered.value+"</td><td>"+data.deaths.value+"</td></tr>"
+    fetchCountryData(name){
 
+        var results = document.getElementById('results');
+
+        fetch(`https://covid19.mathdro.id/api/countries/${name}`)
+        .then(response => response.json())
+        .then(data => {
+            var countryData = `<div class='container'>
+
+                <div class='row'>
+                    <div class='col-sm-4'><h1>${data.confirmed.value}</h1></div>
+                    <div class='col-sm-4'><h1>${data.recovered.value}</h1></div>
+                    <div class='col-sm-4'><h1>${data.deaths.value}</h1></div>
+                </div>
+            
+            </div>`
+            console.log(data.confirmed.value);
+            results.innerHTML = countryData;
+
+        },
+        
+        
+        )
     }
+
+
 }
 
-let app =  new App()
-app.getData();
+const app = new CovidApp();
+app.fetchCountries();
